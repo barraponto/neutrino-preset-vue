d92505af00fbc924b3c7717019e99529fce17e27
Capi Etheriel <barraponto@gmail.com>
2017-03-19 05:04:54 -0300

    feat(eslint): support eslint (as provided by neutrino-preset-lint and eslint-plugin-vue)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const arrify = require('arrify');
+const merge = require('deepmerge');
 const VUE_LOADER = require.resolve('vue-loader');
 
 module.exports = ({ config }) => {
@@ -5,4 +7,23 @@ module.exports = ({ config }) => {
     .rule('vue')
     .test(/\.vue$/)
     .loader('vue', VUE_LOADER);
+
+  if (config.module.rules.has('lint')) {
+    // ensure conditions is an array of original values plus our own regex
+    const conditions = arrify(config.module.rule('lint').get('test')).concat([/\.vue$/]);
+    config.module
+      .rule('lint')
+      .test(conditions)
+      .loader('eslint', props => merge(props, {
+        options: {
+          baseConfig: {
+            plugins: ['vue'],
+            env: { node: true },
+            rules: {
+              'vue/jsx-uses-vars': 2,
+            }
+          },
+        },
+      }));
+  }
 };
